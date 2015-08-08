31e1f0e5965dd0062d8fec1bbfb83a80eb29b3b3
bacuongtr <bacuongtr@gmail.com>
2015-08-08 09:00:28 +0700

    wqewqe
    qe

---
+++ BigHero/BigHero/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigHero.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Note { get; set; }
+        public string Location { get; set; }
+        public string S { get; set; }
+    }
+}
