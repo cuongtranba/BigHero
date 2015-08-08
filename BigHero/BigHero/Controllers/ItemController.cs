ad376bf76dd7af1033f27f944cf49f1c5081d771
Travis223 <hoangtan223@gmail.com>
2015-08-08 10:18:41 +0700

    add itemcontroller and view

---
+++ BigHero/BigHero/Controllers/ItemController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BigHero.Models;
+
+namespace BigHero.Controllers
+{
+    public class ItemController : Controller
+    {
+        private UsersContext db = new UsersContext();
+
+        //
+        // GET: /Item/
+
+        public ActionResult Index()
+        {
+            var items = db.Items.Include(i => i.UserProfile);
+            return View(items.ToList());
+        }
+
+        //
+        // GET: /Item/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        //
+        // GET: /Item/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.UserProfiles, "UserId", "UserName");
+            return View();
+        }
+
+        //
+        // POST: /Item/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Items.Add(item);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserId = new SelectList(db.UserProfiles, "UserId", "UserName", item.UserId);
+            return View(item);
+        }
+
+        //
+        // GET: /Item/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.UserProfiles, "UserId", "UserName", item.UserId);
+            return View(item);
+        }
+
+        //
+        // POST: /Item/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(item).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.UserProfiles, "UserId", "UserName", item.UserId);
+            return View(item);
+        }
+
+        //
+        // GET: /Item/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        //
+        // POST: /Item/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Item item = db.Items.Find(id);
+            db.Items.Remove(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
